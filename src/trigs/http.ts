c34d99503236b47153d2569b2fed62200bf03703
Andrew Rothman <andrew@andrewrothman.com>
2018-04-26 20:25:05 -0700

    add http body parser support

--- src/trigs/http.ts
+++ src/trigs/http.ts
@@ -1,6 +1,8 @@
 import Weka, { WekaTrigDef, WekaTriggerAttachInfo } from "@src";
 import * as Koa from "koa";
 import * as pathToRegexp from "path-to-regexp";
+import * as bodyParser from "koa-bodyparser";
+import { Z_ASCII } from "zlib";
 
 const TRIGGER_NAME: string = "http";
 const DEFAULT_HTTP_PORT: number = 3000;
@@ -8,6 +10,12 @@ const DEFAULT_HTTP_PORT: number = 3000;
 export interface WekaHttpOptions {
 	port?: number;
 	healthEndpointEnabled?: boolean;
+	
+	bodyParser?: {
+		formLimit?: string;
+		jsonLimit?: string;
+		strict?: boolean;
+	}
 }
 
 export default class WekaHttp<Context> implements WekaTrigDef<Context> {
@@ -15,6 +23,10 @@ export default class WekaHttp<Context> implements WekaTrigDef<Context> {
 	
 	private healthEndpointEnabled: boolean = true;
 	private port: number = DEFAULT_HTTP_PORT;
+	private enabledTypes: string[] = ["json", "form"];
+	private bodyParserFormLimit: string = "56kb";
+	private bodyParserJsonLimit: string = "1mb";
+	private bodyParserStrict: boolean = true;
 	
 	constructor(weka: Weka<Context>, options?: WekaHttpOptions) {
 		this.port = Number.parseInt(process.env.WEKA_HTTP_PORT || "") || (options || {}).port || this.port;
@@ -23,14 +35,36 @@ export default class WekaHttp<Context> implements WekaTrigDef<Context> {
 			throw new Error("weka-http \"port\" option must be a number");
 		}
 		
-		if ((options || {}).healthEndpointEnabled === false) {
-			this.healthEndpointEnabled = false;
+		if (options !== undefined) {
+			if (options.healthEndpointEnabled === false) {
+				this.healthEndpointEnabled = false;
+			}
+			
+			if (options.bodyParser !== undefined) {
+				if (options.bodyParser.formLimit !== undefined) {
+					this.bodyParserFormLimit = options.bodyParser.formLimit;
+				}
+				
+				if (options.bodyParser.jsonLimit !== undefined) {
+					this.bodyParserFormLimit = options.bodyParser.jsonLimit;
+				}
+				
+				if (options.bodyParser.strict !== undefined) {
+					this.bodyParserStrict = options.bodyParser.strict;
+				}
+			}
 		}
 		
 		this.app = new Koa();
 	}
 	
 	public attach(weka: Weka<Context>): WekaTriggerAttachInfo {
+		this.app.use(bodyParser({
+			jsonLimit: this.bodyParserJsonLimit,
+			formLimit: this.bodyParserFormLimit,
+			strict: this.bodyParserStrict
+		}));
+		
 		this.app.use(async (ctx: Koa.Context) => {
 			// if the health endpoint is enabled, then return a 200 on the "/healthz" path
 			// this is a docker and kubernetes standard that was put in practice by Google
@@ -58,11 +92,23 @@ export default class WekaHttp<Context> implements WekaTrigDef<Context> {
 				if (reqHttpPath.match(reqPathRegex)) {
 					const reqPathValues = reqPathRegex.exec(reqHttpPath)!;
 
-					const args: { [key: string]: any } = {};
+					let args: { [key: string]: any } = {};
+					
+					// pull in path params
 
 					for (let i = 0; i < reqPathKeys.length; i++) {
 						const reqPathKey: pathToRegexp.Key = reqPathKeys[i];
 						args[reqPathKey.name] = reqPathValues[i + 1];
+					}
+					
+					// merge in query params
+					
+					args = { ...args, ...ctx.query };
+					
+					// merge in body parser params (if correct content-type specified)
+					
+					if (ctx.get("content-type") === "application/json" || ctx.get("content-type") === "application/x-www-form-urlencoded") {
+						args = { ...args, ...ctx.request.body };
 					}
 
 					const funcRes = await weka.invoke({
